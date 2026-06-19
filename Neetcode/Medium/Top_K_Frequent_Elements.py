bbc876fedf9a6037b58f1c105cfc5237aa8292df
Arda-Arancioglu <ardaarancioglu@gmail.com>
2026-06-19 19:15:41 +0300

    Solved Top_K_Frequent_Elements

---
+++ Neetcode/Medium/Top_K_Frequent_Elements.py
@@ -0,0 +1,41 @@
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        myMap= {}
+        myList=[]
+        finalList=[]
+        for i in nums:
+            if i not in myMap:
+                myMap[i] = 1
+            else:
+                myMap[i] += 1
+        total = k
+        biggest=[0]*k+[1] 
+        bestkey=0
+        bestval=0
+
+        while total > 0: 
+            bestkey=0
+            bestval=0       
+            for key,val in myMap.items():
+                if val > bestval:
+                    bestkey = key
+                    bestval = val
+            finalList.append(bestkey)    
+            myMap[bestkey] = -1    
+
+            total-=1
+
+
+        return finalList        
+
+
+        
+
+
+
+
+
+nums = [1,3,4,4,2,7,4,55,5,5]
+k=2
+mySol = Solution()
+print(mySol.topKFrequent(nums,k))
