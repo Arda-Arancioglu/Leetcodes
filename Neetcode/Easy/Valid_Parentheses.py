1346a01bab95b3e9e00439aef94283a36cbb9fbe
Arda-Arancioglu <ardaarancioglu@gmail.com>
2026-06-24 19:27:01 +0300

    Solved Valid_Paranthesis

---
+++ Neetcode/Easy/Valid_Parentheses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        for str in s:
+             
+            if str == "(" or str == "[" or str == "{" :
+                print("appending",str,"to:\n",stack)
+                stack.append(str)
+            else:
+                if len(stack)< 1 :
+                    return False
+                else:    
+                    if str == ")" and stack[-1] == "(":
+                        stack.pop()
+                    elif str == "}"and stack[-1] == "{":
+                        stack.pop()
+                    elif str == "]"and stack[-1] == "[":
+                        stack.pop()
+                    else:
+                        return False    
+        if len(stack) > 0:
+            return False         
+        else:            
+            return True        
+                        
+                 
+     
+     
+s="[]"
+mySol = Solution()
+print(mySol.isValid(s))        
